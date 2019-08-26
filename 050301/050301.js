7f799d5d5f53e5078f7ab471e5621bbf7d517b05
MrNivorous <karn.sood@gmail.com>
2019-08-26 11:22:28 +0700

    050301

--- 050301/050301.js
+++ 050301/050301.js
@@ -2,4 +2,4 @@ let main = document.getElementById('main')
 
 console.log(main)
 
-document.getElementById('content').innerHTML = '<p>Main Content</p>'
+document.getElementById('content').innerHTML = 'Main Content'
