fd14a3d3d2766a33fdaa6786792e1faa3ccd086c
MrNivorous <karn.sood@gmail.com>
2019-08-26 11:25:06 +0700

    050303

--- 050301/050301.js
+++ 050301/050301.js
@@ -3,3 +3,5 @@ let main = document.getElementById('main')
 console.log(main)
 
 document.getElementById('content').innerHTML = 'Main Content'
+
+document.getElementById('products').innerHTML = '<div>1</div><div>2</div>'
